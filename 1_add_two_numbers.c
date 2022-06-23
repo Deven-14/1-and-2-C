ae2edc5213dab1c39aee47700733061123e5dfae
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-06-23 23:39:15 +0530

    Used two scanf functions to get the integers

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -4,8 +4,10 @@ int main()
 {
     int n1, n2;
     //n1 and n2 are the two numbers which are being added
-    printf("Program to add two numbers\nEnter two spaced integers: ");
-    scanf("%d %d", &n1, &n2);
+    printf("Program to add two numbers\nEnter Number 1: ");
+    scanf("%d", &n1);
+    printf("Enter Number 2: ");
+    scanf("%d", &n2);
     printf("Sum of %d and %d = %d\n",n1, n2, n1+n2);
     return 0;
 }
