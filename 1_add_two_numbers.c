3516df4f0f478b81a868a59b4b3fd121068229dc
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-06-23 23:42:18 +0530

    Used one more printf function

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -4,7 +4,8 @@ int main()
 {
     int n1, n2;
     //n1 and n2 are the two numbers which are being added
-    printf("Program to add two numbers\nEnter Number 1: ");
+    printf("Program to add two numbers\n");
+    pritnf("Enter Number 1: );
     scanf("%d", &n1);
     printf("Enter Number 2: ");
     scanf("%d", &n2);
