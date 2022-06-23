fc0dfead5149a6437decd2f3a66e32139f5b9ea3
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-06-23 23:46:38 +0530

    Made it more readable

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -5,9 +5,9 @@ int main()
     int n1, n2;
     //n1 and n2 are the two numbers which are being added
     printf("Program to add two numbers\n");
-    printf("Enter Number 1: ");
+    printf("Enter Number 1 to be added: ");
     scanf("%d", &n1);
-    printf("Enter Number 2: ");
+    printf("Enter Number 2 to be added: ");
     scanf("%d", &n2);
     printf("Sum of %d and %d = %d\n",n1, n2, n1+n2);
     return 0;
