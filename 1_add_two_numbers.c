4d5e21d8ff62fef5b3a0a22d17d20190d7629277
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-06-23 23:36:46 +0530

    mentioned the user prompts

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -4,8 +4,9 @@ int main()
 {
     int n1, n2;
     //n1 and n2 are the two numbers which are being added
+    printf("Program to add two numbers\nEnter two spaced integers: ");
     scanf("%d %d", &n1, &n2);
-    printf("%d\n", n1+n2);
+    printf("Sum of %d and %d = %d\n",n1, n2, n1+n2);
     return 0;
 }
   
