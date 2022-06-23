0b39f39e91baf6bdef59b0ddfe83355dd161bf4d
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-06-23 23:44:49 +0530

    missed the closing qoutes

--- 1_add_two_numbers.c
+++ 1_add_two_numbers.c
@@ -5,7 +5,7 @@ int main()
     int n1, n2;
     //n1 and n2 are the two numbers which are being added
     printf("Program to add two numbers\n");
-    printf("Enter Number 1: );
+    printf("Enter Number 1: ");
     scanf("%d", &n1);
     printf("Enter Number 2: ");
     scanf("%d", &n2);
