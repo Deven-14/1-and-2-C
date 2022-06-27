8afae5b613d0d1fd77534b51dfb00643f107d915
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-06-27 22:15:37 +0530

    First Commit

---
+++ 2_Adding_2_numbers_4_function_pass_by_value.c
@@ -0,0 +1,27 @@
+#inlcude<stdio.h>
+
+int input()
+{
+    scanf("%d", &a);
+}
+
+int addition(int num1, int num2)
+{
+    return(num1+num2);
+}
+
+int output(int num1, int num2, int sum)
+{
+    printf("%d+%d=%d", num1, num2, sum);
+}
+
+int main()
+{
+    int num1, num2, sum;
+    num1=input();
+    num2=input();
+    sum= addition(num1, num2);
+    output(num1, num2, sum);
+    return 0;
+}
+    
