cdc79625341c1df72b86559f94f9ac97581ff708
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-07-01 22:45:39 +0530

    added the data_type for formal parameters, declared int num

--- 2_Adding_2_numbers_4_function_pass_by_value.c
+++ 2_Adding_2_numbers_4_function_pass_by_value.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
-int input_ith_num(ith_num)
+int input_ith_num(int ith_num)
 {
+    int num;
     printf("Enter number %d to be added: ", ith_num);
     scanf("%d", &num);
     return num;
 }
 
-int addition(int num1, int num2)
+int add(int num1, int num2)
 {
     return(num1+num2);
 }
@@ -22,7 +23,7 @@ int main()
     int num1, num2, sum;
     num1=input_ith_num(1);
     num2=input_ith_num(2);
-    sum=addition(num1, num2);
+    sum=add(num1, num2);
     output(num1, num2, sum);
     return 0;
 }
