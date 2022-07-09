7e832a1132298fd32fcb2b46df46c78166554dd6
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-07-09 23:18:10 +0530

    intending the code, corrected the variable num3 in main, changed the logic

--- 4_compare_3_num_pass_by_value.c
+++ 4_compare_3_num_pass_by_value.c
@@ -10,17 +10,17 @@ int input_ith_num(int ith_num)
 
 int compare(int num1, int num2, int num3)
 {
-		if(num1>num2 && num1>num3)
-				return num1;
-		else if(num2>num1 && num2>num3)
-				return num2;
-		else if(num3>num1 && num3>num2)
-				return num3;
+	if(num1>num2 && num1>num3)
+		return num1;
+	else if(num2>num3)
+		return num2;
+	else
+		return num3;
 }
 
 void output(int max_num)
 {
-		printf("The Greatest number is %d\n", max_num);
+	printf("The Greatest number is %d\n", max_num);
 }
 
 int main()
@@ -28,7 +28,7 @@ int main()
 	int num1, num2, num3, max_num;
 	num1=input_ith_num(1);
 	num2=input_ith_num(2);
-	num2=input_ith_num(3);
+	num3=input_ith_num(3);
 	max_num=compare(num1, num2, num3);
 	output(max_num);
 }
