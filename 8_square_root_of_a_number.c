a2ffff4bff1207bcfc3e666c0e9a1fb2471eadd8
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-07-27 22:14:56 +0530

    changed variable name

--- 8_square_root_of_a_number.c
+++ 8_square_root_of_a_number.c
@@ -14,16 +14,16 @@ int square_root(int num)
     return(sqrt(num));
 }    
 
-void output_root(int num, int root)
+void output_root(int num, int sq_root_of_num)
 {
-    printf("Square root of %d is %d", num, root);
+    printf("Square root of %d is %d", num, sq_root_of_num);
 }
 
 int main()
 {
-    int num, root;
+    int num, sq_root_of_num;
     num=input_num();
-    root=square_root(num);
-    output_root(num, root);
+    sq_root_of_num=square_root(num);
+    output_root(num, sq_root_of_num);
     return 0;
 }
