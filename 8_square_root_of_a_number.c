fd3c688135dc1dd039de5b95e0ec4a12e2904cc9
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-07-27 22:10:02 +0530

    First commit

---
+++ 8_square_root_of_a_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int input_num()
+{   
+    int num;
+    printf("Enter a number to find its square root: ");
+    scanf("%d", &num);
+    return num;
+}
+
+int square_root(int num)
+{
+    return(sqrt(num));
+}    
+
+void output_root(int num, int root)
+{
+    printf("Square root of %d is %d", num, root);
+}
+
+int main()
+{
+    int num, root;
+    num=input_num();
+    root=square_root(num);
+    outpur_root(num, root);
+    return 0;
+}
