e4971557a5942315f773744356b817f5b5177fec
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-07-27 22:17:12 +0530

    changed int to float for root variable

--- 8_square_root_of_a_number.c
+++ 8_square_root_of_a_number.c
@@ -14,16 +14,17 @@ int square_root(int num)
     return(sqrt(num));
 }    
 
-void output_root(int num, int sq_root_of_num)
+void output_square_root_of_num(int num, float sq_root_of_num)
 {
     printf("Square root of %d is %d", num, sq_root_of_num);
 }
 
 int main()
 {
-    int num, sq_root_of_num;
+    int num;
+    float sq_root_of_num;
     num=input_num();
     sq_root_of_num=square_root(num);
-    output_root(num, sq_root_of_num);
+    output_square_root_of_num(num, sq_root_of_num);
     return 0;
 }
