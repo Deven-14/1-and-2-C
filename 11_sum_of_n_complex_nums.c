f221fadac783688a344946fe8dd0193900c44f7e
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-08-03 22:22:12 +0530

    First Commit

---
+++ 11_sum_of_n_complex_nums.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int input_number_of_comples_nos()
+{
+    int n;
+    printf("Enter the number of complex numbers to be added: ");
+    scanf("%d", &n);
+    return n;
+}
+
+struct complex
+{
+    float real, imag;
+};
+
+typedef struct complex Complex
+
+void input_ith_complex_num(int n, Complex *num)
+{  
+    for(int i=1; i<=n; i++)
+    { 
+        printf("Enter the real part of the %dth complex number: ", n);
+        scanf("%f", num[i].real);
+        printf("Enter the imaginary part of the %dth complex number: ", n);
+        scanf("%f", num[i].imag);
+    }
+}
+
+Complex addition(int n, Complex *num)
+{
+    Complex sum;
+    sum.real=0;
+    sum.imag=0;
+    for(int  i=1; i<=n; i++)
+    {
+        sum.real+=num[i].real;
+        sum.imag+=num[i].imag;
+    }
+    return sum;
+}
+
+void output(int n, Complex sum)
+{
+    printf("Sum of entered %d complex numbers is %d + i%d\n", n, sum.real, sum.imag);
+}
+
+int main()
+{
+    Complex sum;
+    int n, i;
+    n=input_number_of_comples_nos();
+    Complex num[n];
+    input_ith_complex_num(n, num);
+    sum=addition(n, num);
+    output(n, sum);
+    return 0;
+}
