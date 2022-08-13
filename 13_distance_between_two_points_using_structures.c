44b3c791a36cb34a5459e66a5a39234a3cfa01d8
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-08-13 18:41:21 +0530

    added the points in output()
    points will be displayed in the console along with the distance

--- 13_distance_between_two_points_using_structures.c
+++ 13_distance_between_two_points_using_structures.c
@@ -23,9 +23,9 @@ float distance_formula(Point p1, Point p2)
     return(sqrt(pow((p2.x-p1.x), 2)+pow((p2.y-p1.y), 2)));
 }
 
-void output(float distance)
+void output(float distance, Point p1, Point p2)
 {
-    printf("The distance between the entered points is %f\n", distance);
+    printf("The distance between the points (%d, %d) and (%d, %d) is %f\n", p1.x, p1.y, p2.x, p2.y, distance);
 }
 
 int main()
@@ -33,6 +33,6 @@ int main()
     Point p1= input_co_cordinates_of_ith_point(1);
     Point p2= input_co_cordinates_of_ith_point(2);
     float distance = distance_formula(p1, p2);
-    output(distance);
+    output(distance, p1, p2);
     return 0;
 }
