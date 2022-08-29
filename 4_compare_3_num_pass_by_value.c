dc3c8a75d24a7519795f06802780c74348a8cb1b
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-08-29 21:25:05 +0530

    displaying prompts with ordinals
    1st
    2nd 
    3rd

--- 4_compare_3_num_pass_by_value.c
+++ 4_compare_3_num_pass_by_value.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int input_ith_num(int ith_num)
+int input_ith_num(int ith_num, char ordinal[3])
 {
 	int num;
-	printf("Enter number %d to find the largest of 3 numbers: ", ith_num);
+	printf("Enter %d%s number to find the largest of 3 numbers: ", ith_num, ordinal);
 	scanf("%d", &num);
 	return num;
 }
@@ -26,9 +26,9 @@ void output(int max_num)
 int main()
 {
 	int num1, num2, num3, max_num;
-	num1=input_ith_num(1);
-	num2=input_ith_num(2);
-	num3=input_ith_num(3);
+	num1=input_ith_num(1, "st");
+	num2=input_ith_num(2, "nd");
+	num3=input_ith_num(3, "rd");
 	max_num=max(num1, num2, num3);
 	output(max_num);
 }
