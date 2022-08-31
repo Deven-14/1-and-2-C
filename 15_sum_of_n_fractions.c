b3d720076950b4ebecffc127967ca97b613059bb
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-08-31 23:02:58 +0530

    f[n] -> numbers[n]

--- 15_sum_of_n_fractions.c
+++ 15_sum_of_n_fractions.c
@@ -5,7 +5,7 @@ int gcd(int x, int y)
     while(y!=0)
     {
         int rem = x%y;
-        x=y
+        x=y;
         y=rem;
     }
     return x;
@@ -26,14 +26,14 @@ int input_number_of_fractions()
     return n;
 }
 
-void input_n_fractions(int n, Fraction *f)
+void input_n_fractions(int n, Fraction *numbers)
 {
     for(int i=0; i<n; i++)
     {
         printf("Enter the numerator of fraction %d: ", i+1);
-        scanf("%d", &f[i].num);
+        scanf("%d", &numbers[i].num);
         printf("Enter the denominator of fraction %d: ", i+1);
-        scanf("%d", &f[i].denum);
+        scanf("%d", &numbers[i].denum);
     }
 }
 
@@ -45,33 +45,33 @@ Fraction simplify_fraction(Fraction f)
     return f;
 }
 
-Fraction addition(int n, Fraction *f)
+Fraction addition(int n, Fraction *numbers)
 {
     Fraction sum;
-    sum.denum = f[0].denum*f[1].denum;
-    sum.num = f[0].num*f[1].denum + f[1].num*f[0].denum;
+    sum.denum = numbers[0].denum*numbers[1].denum;
+    sum.num = numbers[0].num*numbers[1].denum + numbers[1].num*numbers[0].denum;
     for(int i=2; i<n; i++)
     {
-        sum.num = f[i].num*sum.denum + sum.num*f[i].denum;
-        sum.denum = sum.denum*f[i].denum;
+        sum.num = numbers[i].num*sum.denum + sum.num*numbers[i].denum;
+        sum.denum = sum.denum*numbers[i].denum;
     }
     sum = simplify_fraction(sum);
     return sum;
 }
 
-void output(int n, Fraction *f, Fraction sum)
+void output(int n, Fraction *numbers, Fraction sum)
 {
     for(int i=0; i<n-1; i++)
-        printf("%d/%d + ", f[i].num, f[i].denum);
-    printf("%d/%d = %d/%d\n", f[n-1].num, f[n-1].denum, sum.num, sum.denum);
+        printf("%d/%d + ", numbers[i].num, numbers[i].denum);
+    printf("%d/%d = %d/%d\n", numbers[n-1].num, numbers[n-1].denum, sum.num, sum.denum);
 }
 
 int main()
 {
     int n= input_number_of_fractions();
-    Fraction f[n];
-    input_n_fractions(n, f);
-    Fraction sum= addition(n, f);
-    output(n, f, sum);
+    Fraction numbers[n];
+    input_n_fractions(n, numbers);
+    Fraction sum= addition(n, numbers);
+    output(n, numbers, sum);
     return 0;
 }
